9673ddac7e2d81eae08dc6feb91134c41656037f
Kurban12A <95amosag1234ff>
2022-07-08 22:26:23 +0300

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+from functions_my import func_circles, funck_rectangle, funk_triangle
+
+print(func_circles.my_circle_square(5))
+print(func_circles.my_circle_perimetr(5))
+print(funck_rectangle.my_reactangle_square(5, 10))
+print(funck_rectangle.my_reactangle_perimetr(5, 10))
+print(funk_triangle.my_triange_square(5, 10))
+print(funk_triangle.my_triange_perimetr(10, 10, 10))
